a9842fc64b5b14d2d5c7bb3ee0ecf55de69c9322
ilias <ilaiskarimalis@outlook.com>
2019-10-17 18:25:01 -0700

    completed getListOfTicks

--- MarkovChain.hs
+++ MarkovChain.hs
@@ -1,6 +1,7 @@
 module MarkovChain
-( importFile
-,
+( getFile,
+  importThenBuild,
+  getListOfTick
 ) where
 
 import Codec.Midi
@@ -9,24 +10,43 @@ import Data.String
 getFile :: FilePath -> IO (Either String Midi)
 getFile filepath = importFile filepath
 
+
+
+{- THIS IS NOT AT ALL COMPLETE IM JUST USING IT TO TEST
+   -}
+importThenBuild :: FilePath -> IO [Char] 
 importThenBuild filepath = do
   x <- (getFile filepath)
   case x of 
     Left x -> return "fail"
-    Right x -> return (show x) 
-  
-{- getTicks returns a list of pairs of channel listOf Ticks pairs
-  - where the listOf Ticks for a certain channel is in the sequential
-  - order in which it appears in the midi file. 
-  -}
---getTicks :: Midi -> [(Channel, [Ticks])]
-getTicks midi = map (\(t, msg)->((channel msg),[t])) (filterNoteOnOff midi)
+    Right x -> return (show (getListOfTick x)) 
+
+
+
+--getListOfKey midi;
+--getListOfVelocity midi;
+
+
+
+{- getListOfTick returns a listOf Ticks which occur in the midi filename
+   -}
+getListOfTick :: Midi -> [Ticks]
+getListOfTick midi = map (\(y,x) -> y) (filterNoteOnOff midi)
 
 
 
 {- filterNoteOnOff returns a listOf tracks wherein every track has
   - a message of type NoteOn or NoteOff
   -}
+filterNoteOnOff :: Midi -> [(Ticks, Message)]
 filterNoteOnOff midi = filter (\(y,x) -> ((isNoteOn x) ||
                                          (isNoteOff x)))
-                                         (tracks midi)
+                                         (mergeLists (tracks midi))
+
+
+
+{- mergeLists merges a ListOf ListOf a into a ListOf a
+   -}
+mergeLists :: Foldable t => t [a] -> [a]
+mergeLists lst = foldl (\x res -> x ++ res) [] lst
+

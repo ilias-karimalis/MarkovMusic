11b72aa007dcfd5862b7258e38adf80d3326469b
ilias <ilaiskarimalis@outlook.com>
2019-10-17 22:29:42 -0700

    fixed populate

--- 312proj.hs
+++ 312proj.hs
@@ -1,8 +1,5 @@
 --main prediction function encapsulates markov chain functions--
 
---synthesize:: [ (channel, [key])] -> [(channel, [velocity])] -> [(channel, [tick])] -> [(channel, [(velocity, key, tick)])]
-
-synthesize [] = []
 
 
 
@@ -13,19 +10,34 @@ matrix [] = []
 matrix list = [(key1, key2, 0) | key1 <- (uniques list), key2 <- (uniques list)]
 
 
+
+  {-
+picknext item [(key1, key2, prop)] =
+
+where 
+    options = filter (match1 item) [(key1, key2, prop)]
+
+match1 (key1, key2, prop) = item == key1
+
+pickfirst list = list!!(round ((length list) * RANDOM))
+-}
+
+
+
 --uses matrix to create a matrix of zeroes and then replaces the zeroes with probabilites
---populate:: Num int => [int] -> [(int, int, int)] -> [(int, int, int)]
-populate [] [(key1, key2, prop)] = [(key1, key2, prop)]
-populate (h:t) [] = []
-populate (h:t) [(key1, key2, prop)]
-    | (length (h:t)) == 0 = [(key1, key2, prop)]
-    | (length (h:t)) == 1 = (map (add1key1 h) [(key1, key2, prop)])
-    | otherwise = populate t (map (add1key1key2 h (head t)) [(key1, key2, prop)])
+--populate:: Eq int Num prop=> [int] -> [(int, int, prop)] -> [(int, int, prop)]
+--goes through a list of keys, velocities, or ticks
+--filters unique items in that list
+--builds triplets such that each unique pair has a triplet, originally created with a zero
+--goes through 
 
 
---populate [] mat = mat
---populate (h:[]) [(key1, key2, prop)] = populate [] (map (add1key1 h) [(key1, key2, prop)])
---populate (h:n:t) [(key1, key2, prop)] = populate (n:t) (map (add1key1key2 h n) [(key1, key2, prop)])
+
+populate :: (Num a, Eq a) => [a] -> [(a,a,a)] -> [(a,a,a)]
+populate [] lst = lst
+populate (h:[]) lst = map (add1key1 h) lst
+populate (h:h2:t) lst = populate (h2:t) (map (add1key1key2 h h2) lst)
+
 
 
 --add1key1:: Eq a => a -> (a, a, a) -> (a, a, a)

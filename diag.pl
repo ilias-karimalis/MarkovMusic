3122fc8a623e2f1f3707938abe74224bca4be633
k8y1b <56333166+k8y1b@users.noreply.github.com>
2019-11-24 12:20:29 -0800

    Update diag.pl

--- diag.pl
+++ diag.pl
@@ -1,6 +1,6 @@
 disease(Rabies).
 symptoms(Ln, Rabies) :- member(aggressiveness, Ln). 
-symptoms(Ln, Rabies) :- write("Y/N have you been bitten by an animal within the last 20 to 60 days?"),
+symptoms(Ln, Rabies) :- write("y/n have you been bitten by an animal within the last 20 to 60 days?"),
      readln(Newln), member(y, Newln).
 
 q(Ans) :- 
